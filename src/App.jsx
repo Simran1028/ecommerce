56bad395f7ed0d29e304970234de94e0a0fd7236
Simran1028 <simrandeepkaur06488@gmail.com>
2025-03-21 23:35:21 +0530

    folder structure

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/home/Home'
+import Dashboard from './pages/admin/dashboard/Dashboard'
+import Order from './pages/order/Order'
+import NoPage from './pages/nopage/NoPage'
+import Cart from './pages/cart/Cart'
+import MyState from './context/data/myState'
+
+const App = () => {
+  return (
+    <MyState>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/admin' element={<Dashboard />} />
+          <Route path='/order' element={<Order />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/*' element={<NoPage />} />
+        </Routes>
+      </BrowserRouter>
+    </MyState>
+  )
+}
+
+export default App
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../../components/layout/Layout'
+import HeroSection from '../../components/herosection/HeroSection'
+
+const Home = () => {
+
+    return (
+        <Layout>
+            <HeroSection/>
+        </Layout>
+
+    )
+}
+
+export default Home
