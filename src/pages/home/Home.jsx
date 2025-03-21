00483176c741756cec3910526667d57de1bfb52f
Simran1028 <simrandeepkaur06488@gmail.com>
2025-03-22 00:03:48 +0530

    Product card created

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Layout from '../../components/layout/Layout'
 import HeroSection from '../../components/herosection/HeroSection'
+import Filter from '../../components/filter/Filter'
+import ProductCard from '../../components/productcard/ProductCard'
 
 const Home = () => {
 
     return (
         <Layout>
             <HeroSection/>
+            <Filter/>
+            <ProductCard/>
         </Layout>
 
     )
